d7341752d2c7c892f7ed04d1afdf6b3ba5fdb834
Alex Kern <alex@kern.io>
2018-10-13 18:17:58 -0700

    Add better gen-docs.sh

--- bin/gen-docs.sh
+++ bin/gen-docs.sh
@@ -6,11 +6,15 @@ cp README.template.md README.md
 
 # some bash magic to extract TOC
 mkdir -p /tmp/debug-solidity
+echo > /tmp/debug-solidity/ref.md
 for c in $CONTRACTS; do
   solmd contracts/$c.sol --dest /tmp/debug-solidity/$c.md
   TOC_LINES=`cat /tmp/debug-solidity/$c.md | grep '^\s*\*' | wc -l | tr -d '[:space:]'`
-  cat /tmp/debug-solidity/$c.md | head -n "$TOC_LINES" >> README.md
-  cat /tmp/debug-solidity/$c.md | tail -n "+$TOC_LINES" >> /tmp/debug-solidity/ref.md
+  cat /tmp/debug-solidity/$c.md | head -n "$(($TOC_LINES + 1))" >> README.md
+  cat /tmp/debug-solidity/$c.md | tail -n "+$(($TOC_LINES + 1))" >> /tmp/debug-solidity/ref.md
   echo >> /tmp/debug-solidity/ref.md
 done
+echo >> README.md
+echo "---" >> README.md
+echo >> README.md
 cat /tmp/debug-solidity/ref.md >> README.md
